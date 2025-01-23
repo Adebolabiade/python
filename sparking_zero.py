2640edc69ac53f0c8fe7f7514c319f15c2c92501
adebola <adebolaadelekeagunbiade@gmail.com>
2025-01-23 12:01:00 +0000

    update

--- sparking_zero.py
+++ sparking_zero.py
@@ -19,6 +19,41 @@ b = 5
 print(b+a)
 print(b-a)
 print(b/a)
+print(b%a)x=10
+y="jerry"
+print(type(x))
+print(type(y))
+
+x = "python"
+y = "is"
+z ="awesome"
+print(x+y+z)
+print(y)
+print(z)
+
+print("adebola")
+print(16)
+print("Red")
+
+a = 5
+b = 5
+print(b+a)
+print(b-a)
+print(b/a)
 print(b%a)
+print(b*a)
+print(b**a)
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
 print(b*a)
 print(b**a)
