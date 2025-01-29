8455f1f81fc7788b3eb4dc193ef55083e2949b7a
adebola <adebolaadelekeagunbiade@gmail.com>
2025-01-29 14:46:25 +0000

    update

---
+++ Task3.py
@@ -0,0 +1,4 @@
+x = "The echoes of the chariots trasversed, the cave in which we took refuge, as we shook with the fear of death from our very bones"
+print(x.upper())
+print(x.replace("e","f"))
+print(x.split(","))
