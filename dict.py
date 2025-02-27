a3f099446ac7bef5fdfea86cbb903780566935ff
adebola <adebolaadelekeagunbiade@gmail.com>
2025-02-27 12:01:53 +0000

    update

---
+++ dict.py
@@ -0,0 +1,36 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+
+thisdict = {
+  "brand": "Ford",
+  "password": "Mustang",
+  "year": 1964
+}
+print(thisdict["password"])
+
+
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+print(thisdict["electric"])
+x = thisdict.keys()
+print(x)
+
+
+
+
+
+
+
+
+
---
+++ sets_dict_task.py
@@ -0,0 +1,22 @@
+movie_suggestions={"onward", 
+"the conjuring", 
+"wedding party",
+"rise of tmnt", 
+"violent night", 
+"onward"
+}
+print(movie_suggestions)
+
+movie_details={
+    "jackass":5, 
+    "avengers":8, 
+    "akira":7, 
+    "die hard": 6
+}
+print(movie_details)
+
+movie_suggestions.update({"huntsman"})
+print(movie_suggestions)
+
+
+
