e03567344f17c00df10ecb02d738b4865efaac92
adebola <adebolaadelekeagunbiade@gmail.com>
2025-03-12 14:46:16 +0000

    update

---
+++ if_else.py
@@ -0,0 +1,49 @@
+a = 33 #standard if statement
+b = 200
+
+if b > a:
+  print("b is greater than a")
+
+
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a") #APPLICATION OF CONDITIONS AND OUTCOMES
+elif a == b:
+  print("a and b are equal")
+
+
+
+
+
+  a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal") #Equals to statements
+else:
+  print("a is greater than b")
+
+
+
+#shorthand IF ELSE
+  if a > b: print("a is greater than b") 
+
+
+  a=200
+  b=33
+  c=50
+  if a>b or c>a:
+    print("both conditions are true")
+
+
+
+
+a=33
+b= 200
+if not a>b:
+    print("a is not greater thann b")
+
+
